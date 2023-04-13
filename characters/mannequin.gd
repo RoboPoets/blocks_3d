afbf64559da5a93e175d07ad86683e6628b44efd
Andreas Raddau <raddau@robopoets.com>
2023-04-13 16:44:50 +0200

    Added support for footstep fx to mannequin animations

--- characters/mannequin.gd
+++ characters/mannequin.gd
@@ -18,6 +18,14 @@ extends CharacterBody3D
 ## The accumulated input vector for this character.
 var current_input_vector:Vector3 = Vector3.ZERO
 
+## Collects all traces that need to be performed during physics processing.
+var trace_queue:Array
+
+## Cache the name of the previous foot step anim notify.
+## This is needed as a workaroud around the fact that walking and
+## running animations are blended and the foot step notifies of both
+## animations fire when they're active.
+var last_foot_down:StringName
 
 func _ready():
 	set_process_unhandled_input(current)
@@ -25,8 +33,8 @@ func _ready():
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
-## Handles character movement and rotation.
 func _physics_process(delta):
+	# move character in the direction of the input vector
 	var current_vel:Vector3 = velocity
 	var current_grav:float = current_vel.y
 	var view_transform: Transform3D = global_transform
@@ -44,6 +52,7 @@ func _physics_process(delta):
 	set_velocity(current_vel)
 	move_and_slide()
 
+	# Rotate character towards movement direction
 	if current_input_vector != Vector3.ZERO && velocity != Vector3.ZERO:
 		var up:Vector3 = global_transform.basis.y
 		var forward:Vector3 = -global_transform.basis.z
@@ -53,7 +62,17 @@ func _physics_process(delta):
 		var limit:float = delta * deg_to_rad(rotation_rate)
 		rotate(up, clamp(angle, -limit, limit))
 
+	# Pass data to animation state machine
 	$AnimationTree.speed = velocity.length()
+
+	# Process the trace queue
+	var space:PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
+	while not trace_queue.is_empty():
+		var trace:Dictionary = trace_queue.pop_back()
+		if trace.id == &"step":
+			var result:Dictionary = space.intersect_ray(trace.params)
+			if not result.is_empty():
+				result.collider.emit_signal("touched", result.position)
 
 
 ## Handles all the player input coming from connected input devices.
@@ -63,3 +82,19 @@ func _unhandled_input(event):
 	vec.z = Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
 	current_input_vector = vec.normalized() if vec.length() > 1 else vec
 
+
+func on_anim_foot_down(bone:StringName):
+	if bone == last_foot_down:
+		return
+
+	var skel:Skeleton3D = $Skeleton/Skeleton3D
+	var bone_idx:int = skel.find_bone(bone)
+	if bone_idx < 0:
+		return
+
+	var pos:Vector3 = (skel.global_transform * (skel.get_bone_global_pose(bone_idx))).origin
+	var params:PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.new()
+	params.from = pos
+	params.to = pos + Vector3(0,-0.5,0)
+	trace_queue.append({ "id": &"step", "params": params })
+	last_foot_down = bone
