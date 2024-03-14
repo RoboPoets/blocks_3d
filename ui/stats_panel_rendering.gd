6559f7d55d7bbee7b5d1bd5b67bc7fe737eb3a07
Andreas Raddau <raddau@robopoets.com>
2024-03-14 21:01:10 +0100

    Added keyboard shortcuts to toggle some rendering features during gameplay

--- ui/stats_panel_rendering.gd
+++ ui/stats_panel_rendering.gd
@@ -1,11 +1,16 @@
 extends Control
 
+@export_category("Stats Panel")
+@export var enable_input:bool = false
 
 var gpu_time:float = 0.0
 var draw_time:float = 0.0
 var game_time:float = 0.0
 
 var _viewports:Array[RID] = []
+
+var _probes_enabled:bool = true
+var _gi_enabled:bool = true
 
 
 func _enter_tree():
@@ -26,6 +31,8 @@ func _ready():
 	for rid in _viewports:
 		RenderingServer.viewport_set_measure_render_time(rid, true)
 
+	set_process_unhandled_key_input(enable_input)
+
 
 func _process(delta:float):
 	var gpu:float = 0.0
@@ -41,6 +48,21 @@ func _process(delta:float):
 	%Game.text = "%.2f ms" % game_time
 	%Draw.text = "%.2f ms" % draw_time
 	%GPU.text = "%.2f ms" % gpu_time
+
+
+func _unhandled_key_input(event:InputEvent):
+	if not event.is_released():
+		return
+
+	if event.keycode == KEY_F2:
+		_probes_enabled = not _probes_enabled
+		for n in get_tree().get_root().find_children("", "ReflectionProbe", true, false):
+			n.visible = _probes_enabled
+
+	if event.keycode == KEY_F3:
+		_gi_enabled = not _gi_enabled
+		for n in get_tree().get_root().find_children("", "WorldEnvironment", true, false):
+			n.environment.sdfgi_enabled = _gi_enabled
 
 
 func add_viewport(rid:RID):
